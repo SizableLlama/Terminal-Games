faea4d329484c8237bd8eee5846c77c105a382dd
SizableLlama <williamagrobb@gmail.com>
2026-06-30 13:44:23 +0100

    Refactor game selection menu and main loop

--- main.cpp
+++ main.cpp
@@ -3,22 +3,47 @@
 #include <iostream>
 #include <string>
 
-
-int main() {
+bool select(int& input, bool& play);
+bool menu(bool& play) {
 	int input;
-	std::cout<<"Which game would you like to play?\n---------------\n1 for wordle, 2 for tic-tac-toe\n---------------\n>";
+	std::string message = "Which game would you like to play?\n--------------------------------------------------\n1 for wordle, 2 for tic-tac-toe. (3 to quit.)\n--------------------------------------------------\n>";
+	std::cout<<message;
 	std::cin>>input;
+	while (input != 3) {
+		while (input != 1 && input != 2 && input != 3) {
+			std::cout<<"Please input either 1 or 2 (3 to quit).\n\n"<<message;
+			std::cin>>input;
+		}
 
-	while (input != 1 && input != 2) {
-		std::cout<<"Please input either 1 or 2.\nWhich game would you like to play?\n---------------\n1 for wordle, 2 for tic-tac-toe\n---------------\n>";
-		std::cin>>input;
+		play = select(input, play);
+		break;
 	}
+	std::cout<<"\nGoodbye!\n";
+	return play = false;
+}
 
+bool select(int& input, bool& play) {
 	if (input == 1) {
 		wordle();
+		return true;
 	}
 	else if (input == 2) {
 		tictactoe();
+		return true;
+	}
+	else if (input == 3) {
+		return false;
+	}
+	else return true;
+}
+
+
+
+
+int main() {
+	bool play = true;
+	while (play) {
+		menu(play);
 	}
 	return 0;
 }
